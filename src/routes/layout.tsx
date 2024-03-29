0be6507fff918f456cc3d42a4ad8573eeb5b179b
maiieul <maieul.chevalier@gmail.com>
2024-03-29 22:22:21 +0100

    try with 2 cacheControls

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -10,13 +10,13 @@ import styles from "./styles.css?inline";
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
+  cacheControl({
+    maxAge: 0,
+    sMaxAge: 60,
+  });
   cacheControl(
     {
-      // Always serve a cached response by default, up to a week stale
-
-      // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-      maxAge: 0,
-      sMaxAge: 60,
+      maxAge: 3600,
     },
     "Vercel-CDN-Cache-Control"
   );
